060883924b5a138b02c4cc9415d0296cbf8ddd6e
Colin <cwh@colinwhill.com>
2018-10-16 13:46:14 +0200

    [add] basic todo crud actions

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import db from './db/db';
 // Set up the express app
 const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // get all todos
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
@@ -14,4 +21,117 @@ const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
+});
+
+// Create a single Todo
+app.post('/api/v1/todos', (req, res) => {
+  if(!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    });
+  } else if(!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    });
+  }
+ const todo = {
+   id: db.length + 1,
+   title: req.body.title,
+   description: req.body.description
+ }
+ db.push(todo);
+ return res.status(201).send({
+   success: 'true',
+   message: 'todo added successfully',
+   todo
+ })
+});
+
+// Get a single to do
+app.get('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  db.map((todo) => {
+    if (todo.id === id) {
+      return res.status(200).send({
+        success: 'true',
+        message: 'todo retrieved successfully',
+        todo,
+      });
+    }
+  });
+  return res.status(404).send({
+    success: 'false',
+    message: 'todo does not exist',
+  });
+});
+
+// Update a ToDo
+app.put('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todoFound;
+  let itemIndex;
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      todoFound = todo;
+      itemIndex = index;
+    }
+  });
+
+  if (!todoFound) {
+    return res.status(404).send({
+      success: 'false',
+      message: 'todo not found',
+    });
+  }
+
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required',
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required',
+    });
+  }
+
+  const updatedTodo = {
+    id: todoFound.id,
+    title: req.body.title || todoFound.title,
+    description: req.body.description || todoFound.description,
+  };
+
+  db.splice(itemIndex, 1, updatedTodo);
+
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo added successfully',
+    updatedTodo,
+  });
+});
+
+// Delete a ToDo
+app.delete('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+       db.splice(index, 1);
+       return res.status(200).send({
+         success: 'true',
+         message: 'Todo deleted successfuly',
+       });
+    }
+  });
+
+
+    return res.status(404).send({
+      success: 'false',
+      message: 'todo not found',
+    });
+
+ 
 });
