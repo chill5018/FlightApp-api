2c8004bb8c883ca8cf40b03f3da5a4c819cb7bc1
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-10-16 15:15:32 +0200

    Remove additional Id column

--- server/migrations/20181016130136-create-flight.js
+++ server/migrations/20181016130136-create-flight.js
@@ -8,9 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      id: {
-        type: Sequelize.STRING
-      },
       date: {
         type: Sequelize.DATE
       },
