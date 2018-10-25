c5cb887e22e2b24a7805e8bad414ae34395ffd4f
Diana Buras <burasdiana@hotmail.com>
2018-10-25 14:04:06 +0200

    [add airlines controller

---
+++ server/controllers/airlines.js
@@ -0,0 +1,19 @@
+const Airline = require('../models').Airline;
+
+module.exports = {
+    create(req, res) {
+      return Airline
+        .create({
+          name: req.body.name,
+        })
+        .then((airline) => res.status(201).send(airline))
+        .catch((error) => res.status(400).send(error));
+    },
+  
+    list(req, res) {
+        return Airline
+        .all()
+        .then( airlines => res.status(200).send(airlines))
+        .catch( error => res.status(400).send(error));
+    },
+  };
