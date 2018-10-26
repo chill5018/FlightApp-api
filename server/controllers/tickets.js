e2041c4c62aff133690b4363e5a1a8df6f5d74e8
Diana Buras <burasdiana@hotmail.com>
2018-10-26 14:13:15 +0200

    [add] tickets controller

---
+++ server/controllers/tickets.js
@@ -0,0 +1,20 @@
+const Ticket = require('../models').Ticket;
+
+module.exports = {
+    create(req, res) {
+      return Ticket
+        .create({
+          price: req.body.price,
+          flightIndex: req.body.flightIndex
+        })
+        .then((airline) => res.status(201).send(airline))
+        .catch((error) => res.status(400).send(error));
+    },
+  
+    list(req, res) {
+        return Ticket
+        .all()
+        .then( tickets => res.status(200).send(tickets))
+        .catch( error => res.status(400).send(error));
+    },
+  };
