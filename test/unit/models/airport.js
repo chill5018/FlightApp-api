4c1e378ae0e7d76f694fa681af5ebca0e99091b0
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-10-28 17:56:48 +0100

    Airport unit tests

---
+++ test/unit/models/airport.js
@@ -0,0 +1,38 @@
+import { assert } from 'chai';
+
+import models from 'server/models';
+
+import factories from 'test/factories';
+import truncate from 'test/truncate';
+
+describe('Airport model', () => {
+  let airport;
+
+  beforeEach(async () => {
+    await truncate();
+
+    airport = await factories.airport();
+  });
+
+  it('Airport ID is number', async () => {
+    assert.isNumber(airport.id);
+  });
+
+  it('Airport Name is String', async () => {
+    assert.isString(airport.name);
+  });
+
+  it('Airport code is 3 char string', async () => {
+    assert.isString(airport.code);
+    assert.lengthOf(airport.code, 3);
+    for(var i =0; i < airport.code.length;i++) assert.isNotNumber(airport.code.charAt(i)); 
+
+  });
+  
+
+  it('should truncate the airports table with each test', async () => {
+    const count = await models.Airport.count();
+
+    assert.equal(count, 1);
+  });
+});
