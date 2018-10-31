faa3273fe526fd2dd9b8922d6bc91353da8029b5
Dan Lepadatu <lepadatudan@gmail.com>
2018-10-31 23:44:18 +0100

    test: add flight testing for GET all and CREATE on airline endpoints

--- test/controllers/flights.spec.js
+++ test/controllers/flights.spec.js
@@ -1,13 +1,110 @@
-const chai = require('chai');
+const { Airport, Flight, Airline } = require('../../server/models');
+
 const chaiHttp = require('chai-http');
 
 const server = require('../../app');
+
+const chai = require('chai');
 
 const should = chai.should();
 
 chai.use(chaiHttp);
 
 describe('Flights controller', () => {
-  it('should list ALL flights on /flights GET');
-  it('should add a SINGLE airline on /flights POST');
+  let airlineData;
+  let airportsData;
+  beforeEach((done) => {
+    Airline.create({ name: 'KLM' }).then((airline) => {
+      Airport.bulkCreate([
+        { name: 'Copenhagen Airport', code: 'CPH' },
+        { name: 'John F. Kennedy International Airport', code: 'JFK' },
+      ], { returning: true })
+        .then((airports) => {
+          airlineData = airline;
+          airportsData = airports;
+          Flight.bulkCreate([
+            {
+              airlineIndex: airline.id,
+              flightNumber: 'KLM-2345',
+              departureDateTime: '2018-10-22T21:37:12.012Z',
+              arrivalDateTime: '2018-10-23T21:37:12.012Z',
+              destinationIndex: airports[0].id,
+              originIndex: airports[1].id,
+            },
+            {
+              airlineIndex: airline.id,
+              flightNumber: 'KLM-2346',
+              departureDateTime: '2018-10-29T21:37:12.012Z',
+              arrivalDateTime: '2018-10-30T21:37:12.012Z',
+              destinationIndex: airports[0].id,
+              originIndex: airports[1].id,
+            },
+          ]).then(() => done());
+        });
+    });
+  });
+
+  afterEach((done) => {
+    // We need `where {}` if we want to
+    // remove all the records from the DB
+    // TODO: Refactor this so we don't have infinite chained callbacks
+    Airline.destroy({ where: {}, force: true, truncate: true, cascade: true }).then(() => {
+      Airport.destroy({ where: {}, force: true, truncate: true, cascade: true }).then(() => {
+        Flight.destroy({ where: {}, force: true, truncate: true, cascade: true }).then(() => done());
+      });
+    });
+  });
+
+  it('should list ALL flights on /flights GET', (done) => {
+    chai.request(server)
+      .get('/api/flights')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.eql(2);
+        res.body[0].should.have.property('id');
+        res.body[0].should.have.property('airlineIndex');
+        res.body[0].should.have.property('flightNumber');
+        res.body[0].should.have.property('departureDateTime');
+        res.body[0].should.have.property('arrivalDateTime');
+        res.body[0].should.have.property('destinationIndex');
+        res.body[0].should.have.property('originIndex');
+        res.body[0].should.have.property('updatedAt');
+        res.body[0].should.have.property('createdAt');
+        res.body[0].flightNumber.should.equal('KLM-2345');
+        res.body[1].flightNumber.should.equal('KLM-2346');
+        done();
+      });
+  });
+
+  it('should add a SINGLE airline on /flights POST', (done) => {
+    chai.request(server)
+      .post('/api/flights')
+      .send(
+        {
+          airlineIndex: airlineData.id,
+          flightNumber: 'KLM-2347',
+          departureDateTime: '2018-10-22T21:37:12.012Z',
+          arrivalDateTime: '2018-10-23T21:37:12.012Z',
+          destinationIndex: airportsData[0].id,
+          originIndex: airportsData[1].id,
+        }
+      ).end((err, res) => {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('id');
+        res.body.should.have.property('airlineIndex');
+        res.body.should.have.property('flightNumber');
+        res.body.should.have.property('departureDateTime');
+        res.body.should.have.property('arrivalDateTime');
+        res.body.should.have.property('destinationIndex');
+        res.body.should.have.property('originIndex');
+        res.body.should.have.property('updatedAt');
+        res.body.should.have.property('createdAt');
+        res.body.flightNumber.should.equal('KLM-2347');
+        done();
+      });
+  });
 });
