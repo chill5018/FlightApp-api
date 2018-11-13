5e78313410cc43c65b680825d8b1e3f91e1eb07c
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-11-13 20:54:15 +0100

    Add validation to airline model

--- server/models/airline.js
+++ server/models/airline.js
@@ -2,7 +2,9 @@ module.exports = (sequelize, DataTypes) => {
   const Airline = sequelize.define('Airline', {
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      validate :{
+        is: "^[a-zA-Z ]*$",
+      }
     },
   });
 
