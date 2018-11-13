aecdb64e25b01e2a6a5d3eb109392a1f5943779b
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-11-13 20:55:35 +0100

    Add airline name tests

--- test/unit/models/airline.js
+++ test/unit/models/airline.js
@@ -11,20 +11,34 @@ describe('Airline model', () => {
   beforeEach(async () => {
     await truncate();
 
-    airline = await factories.airline();
-  });
-
-  it('should create Airline ID as number', async () => {
-    assert.isNumber(airline.id);
+    //airline = await factories.airline();
   });
 
   it('should create Airline Name as String', async () => {
-    assert.isString(airline.name);
+    const airlineData = {
+      name: 'Airline',
+    };
+
+    const result = models.Airline.create(airlineData);
+    assert.isOk(result);
+  });
+
+  
+  it('should throw error when Airline Name is number', async () => {
+    const airlineData = {
+      name: 123,
+    };
+
+    models.Airline.create(airlineData).catch((response) => {
+      const [error] = response.errors;
+      assert.equal(error.type, 'Validation error');
+      assert.equal(error.message, 'Validation is on name failed');
+    });
   });
 
   it('should truncate the airlines table with each test', async () => {
     const count = await models.Airline.count();
 
-    assert.equal(count, 1);
+    assert.equal(count, 0);
   });
 });
