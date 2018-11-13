d715fe4393f83b639f05a3d105402d3dd0638788
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-11-13 20:56:00 +0100

    Add ticket tests

--- test/unit/models/ticket.js
+++ test/unit/models/ticket.js
@@ -8,28 +8,54 @@ import truncate from 'test/truncate';
 
 describe('Ticket model', () => {
   let ticket;
+  let flight;
 
   beforeEach(async () => {
     await truncate();
 
-    ticket = await factories.ticket();
+    flight = await factories.flight();
   });
 
-  it('should create Ticket ID as number', async () => {
-    assert.isNumber(ticket.id);
+  it('should create Ticket price as float', async () => {
+    const ticket =  {
+      price: 689.99,
+      flightIndex: flight.id,
+    };
+
+    const result = models.Ticket.create(ticket);
+    assert.isOk(result);
   });
 
-  it('should create Ticket price as number', async () => {
-    assert.isNumber(ticket.price);
+  it('should throw error when Ticket price is int', async () => {
+    const ticket =  {
+      price: 689,
+      flightIndex: flight.id,
+    };
+
+    models.Ticket.create(ticket).catch((response) => {
+      const [error] = response.errors;
+      assert.equal(error.type, 'Validation error');
+      assert.equal(error.message, 'Validation isFloat on name failed');
+    });
   });
 
-  it('should create Flight ID as number', async () => {
-    assert.isNumber(ticket.flightIndex);
+  
+  it('should throw error when Ticket price is string', async () => {
+    const ticket =  {
+      price: '689',
+      flightIndex: flight.id,
+    };
+
+    models.Ticket.create(ticket).catch((response) => {
+      const [error] = response.errors;
+      assert.equal(error.type, 'Validation error');
+      assert.equal(error.message, 'Validation isFloat on name failed');
+    });
   });
 
   it('should truncate the tickets table with each test', async () => {
     const count = await models.Ticket.count();
 
-    assert.equal(count, 1);
+    assert.equal(count, 0);
   });
 });
