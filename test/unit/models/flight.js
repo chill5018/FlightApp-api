0460d7a48041465017b32107d9a067945970855c
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2018-11-13 20:55:54 +0100

    Add flight tests

--- test/unit/models/flight.js
+++ test/unit/models/flight.js
@@ -8,38 +8,115 @@ import truncate from 'test/truncate';
 
 describe('Flight model', () => {
   let flight;
+  let airline;
+  let origin;
+  let destination;
 
   beforeEach(async () => {
     await truncate();
 
-    flight = await factories.flight();
-  });
-
-  it('should create Flight ID as number', async () => {
-    assert.isNumber(flight.id);
+    airline = await factories.airline();
+    origin = await factories.airport();
+    destination = await factories.airport();
+    //flight = await factories.flight();
   });
 
   it('should create Flight as LL + NNN(N) number format', async () => {
-    for (let i = 0; i < 2; i++) {
-      assert.isNotNumber(flight.flightNumber.charAt(i));
-    }
+    const flightData = {
+        airlineIndex: airline.id,
+        flightNumber: 'KLM-2345',
+        departureDateTime: '2018-10-22T21:37:12.012Z',
+        arrivalDateTime: '2018-10-23T21:37:12.012Z',
+        destinationIndex: destination.id,
+        originIndex: origin.id,
+    };
 
-    for (let i = 2; i < flight.flightNumber.length; i++) {
-      const number = parseInt(flight.flightNumber.charAt(i), 10);
-      assert.isNumber(number);
-    }
+    const result = models.Flight.create(flightData);
+    assert.isOk(result);
   });
 
-  it('should create Arrival date after Departure date', async () => {
-    const departureDate = new Date(flight.departureDateTime);
-    const arrivalDate = new Date(flight.arrivalDateTime);
+  it('should throw error when Flight number has the wrong format', async () => {
+    const flightData = {
+        airlineIndex: airline.id,
+        flightNumber: 'KL-M45',
+        departureDateTime: '2018-10-22T21:37:12.012Z',
+        arrivalDateTime: '2018-10-23T21:37:12.012Z',
+        destinationIndex: destination.id,
+        originIndex: origin.id,
+    };
 
-    assert.isTrue(arrivalDate.getFullYear() > departureDate.getFullYear());
+    models.Flight.create(flightData).catch((response) => {
+      const [error] = response.errors;
+      assert.equal(error.type, 'Validation error');
+      assert.equal(error.message, 'Validation is on flightNumber failed');
+    });
+  });
+
+  it('should create departure time as date', async () => {
+    const flightData = {
+        airlineIndex: airline.id,
+        flightNumber: 'KLM-2345',
+        departureDateTime: '2018-10-22T21:37:12.012Z',
+        arrivalDateTime: '2018-10-23T21:37:12.012Z',
+        destinationIndex: destination.id,
+        originIndex: origin.id,
+    };
+
+    const result = models.Flight.create(flightData);
+    assert.isOk(result);
+  });
+
+  it('should throw error when departure time is not date', async () => {
+    const flightData = {
+       airlineIndex: airline.id,
+        flightNumber: 'KLM-2345',
+        departureDateTime: 'tomorrow',
+        arrivalDateTime: '2018-10-23T21:37:12.012Z',
+        destinationIndex: destination.id,
+        originIndex: origin.id,
+    };
+
+    models.Flight.create(flightData).catch((response) => {
+      const [error] = response.errors;
+      assert.equal(error.type, 'Validation error');
+      assert.equal(error.message, 'Validation isDate on departureDateTime failed');
+    });
+  });
+
+  it('should create arrival time as date', async () => {
+    const flightData = {
+        airlineIndex: airline.id,
+        flightNumber: 'KLM-2345',
+        departureDateTime: '2018-10-22T21:37:12.012Z',
+        arrivalDateTime: '2018-10-23T21:37:12.012Z',
+        destinationIndex: destination.id,
+        originIndex: origin.id,
+    };
+
+    const result = models.Flight.create(flightData);
+    assert.isOk(result);
+  });
+
+  it('should throw error when arrival time is not date', async () => {
+    const flightData = {
+        airlineIndex: airline.id,
+        flightNumber: 'KLM-2345',
+        departureDateTime: '2018-10-23T21:37:12.012Z',
+        arrivalDateTime: 'tomorrow',
+        destinationIndex: destination.id,
+        originIndex: origin.id,
+    };
+
+    models.Flight.create(flightData).catch((response) => {
+      const [error] = response.errors;
+      assert.equal(error.type, 'Validation error');
+      assert.equal(error.message, 'Validation isDate on arrivalDateTime failed');
+    });
   });
 
   it('should truncate the flights table with each test', async () => {
-    const count = await models.Airport.count();
+    const count = await models.Flight.count();
 
-    assert.equal(count, 2);
+    assert.equal(count, 0);
   });
 });
