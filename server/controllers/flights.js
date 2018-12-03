4091fb76664458fbcbba2d4c025a483e2086b0d7
Dan Lepadatu <lepadatudan@gmail.com>
2018-12-03 23:53:32 +0100

    feat: return pairs of flights of return city and date are available in the request query

--- server/controllers/flights.js
+++ server/controllers/flights.js
@@ -17,82 +17,90 @@ module.exports = {
       .then((flight) => res.status(201).send(flight))
       .catch((error) => res.status(400).send(error));
   },
-  
-  getById(req,res){
+
+  getById(req, res) {
     return Flight
-    .findOne(
-      {where : {id: req.params.id}}
-    )
-    .then(flight => res.status(200).send(flight))
-    .catch(error => res.status(400).send(error));
+      .findOne(
+        { where: { id: req.params.id } }
+      )
+      .then(flight => res.status(200).send(flight))
+      .catch(error => res.status(400).send(error));
   },
 
   list(req, res) {
-    let dep_city = req.query.departureCity;
-    let arr_city = req.query.arrivalCity;
-    let dep_date = req.query.departureDate;
-    let arr_date = req.query.arrivalDate;
-    
-    // if no query params, return all flights
-    if(dep_date == null || dep_city == null || arr_city == null){
-      return Flight
-      .all()
-      .then(flights => res.status(200).send(flights))
-      .catch(error => res.status(400).send(error));
-    }
-      var getdep_id =
-      Airport
-     .findOne(
-       {where : {name: dep_city}}
-     )
-     .then(function(airport){
-       return airport.id;
-     });
+    const hasQueryParams = !!Object.keys(req.query).length;
 
-     var get_bothIds = 
-     getdep_id.then(function(id){
-      Airport
-      .findOne(
-        {where : {name: arr_city}}
-      )
-      .then(function(airport){
-        return [id, airport.id];
-      })
-      .then(([a,b]) => {
-        
-        return Flight
-        .findAll({
-          where: {[Op.and]: [{departureDateTime : {'$gte': new Date(dep_date)}}, {originIndex : a} ,{destinationIndex : b}]}
-        })
-        .then(flights => {
-          if(arr_date == null){
-            res.status(200).send(flights);
-          }
-          else{
-            return Flight
-            .findAll({
-              where: {[Op.and]: [{departureDateTime : {'$gte': new Date(arr_date)}}, {originIndex : b} ,{destinationIndex : a}]}
-            })
-            .then(results => {
-              return res.status(200).send(flights.concat(results)); //makePairs(flights,results)
-            })
-          }
-        })
+    // Return all flight if there are no query params
+    if (!hasQueryParams) {
+      return Flight
+        .all()
+        .then(flights => res.status(200).send(flights))
         .catch(error => res.status(400).send(error));
-     });
-    });
+    }
+
+    let { departureCity } = req.query;
+    let { arrivalCity } = req.query;
+    let { departureDate } = req.query;
+    let { returnDate } = req.query;
+
+    const isReturn = !!departureCity && !!arrivalCity && !!departureDate && !!returnDate;
+
+    const getDepartureAirportId = Airport.findOne({ where: { name: departureCity } })
+        .then(function (airport) {
+          return airport.id;
+        });
+
+    getDepartureAirportId.then(function (departureAirportId) {
+        Airport
+          .findOne({ where: { name: arrivalCity } })
+          .then(function (arrivalAirport) {
+            return [departureAirportId, arrivalAirport.id];
+          })
+          .then(([a, b]) => {
+            return Flight
+              .findAll({
+                where: { [Op.and]: [{ departureDateTime: { '$gte': new Date(departureDate) } }, { departureDateTime: { '$lt': getNextDay(departureDate) } }, { originIndex: a }, { destinationIndex: b }] }
+              })
+              .then(flights => {
+                if (returnDate == null) {
+                  res.status(200).send(flights);
+                } else {
+                  return Flight
+                    .findAll({
+                      where: { [Op.and]: [{ departureDateTime: { '$gte': new Date(returnDate) } }, { departureDateTime: { '$lt': getNextDay(returnDate) } }, { originIndex: b }, { destinationIndex: a }] }
+                    })
+                    .then(results => {
+                      const formattedData = makePairs(flights, results);
+                      return res.status(200).send(formattedData);
+                    })
+                }
+              }).catch(error => res.status(400).send(error));
+          });
+      });
   },
 }
-function makePairs(l1, l2){
-  var pairs = new Array(l1.lenght * l2.lenght);
-  var pos = 0;
-  
-  for (var i = 0; i < l1.length; i++) {
-    for (var j = 0; j < l2.length; j++) {
-        pairs[pos] = [l1[i], l2[j]];
-        pos++;
+
+function makePairs(departureFlights, returnFlights) {
+  let pairs = [];
+
+  for (let i = 0; i < departureFlights.length; i++) {
+    for (let j = 0; j < returnFlights.length; j++) {
+      const departureFlightResult = departureFlights[i];
+      const returnFlightResult = returnFlights[i];
+
+      const flight = {
+        departureFlight: departureFlightResult,
+        returnFlight: returnFlightResult
+      }
+
+      pairs.push(flight);
     }
+  }
+
+  return pairs;
 }
-return JSON.stringify(pairs);
+
+function getNextDay(date) {
+  return new Date(new Date(date).getTime() + 24 * 60 * 60 * 1000)
 }
-  
+
