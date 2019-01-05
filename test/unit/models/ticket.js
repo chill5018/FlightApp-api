66d9a57fb1dbc25f46621a186c057e27cd8ebba8
Colin <cwh@colinwhill.com>
2019-01-05 12:42:59 +0100

    [add] boundary value analysis to ticket prices

--- test/unit/models/ticket.js
+++ test/unit/models/ticket.js
@@ -51,4 +51,39 @@ describe('Ticket model', () => {
       assert.equal(error.message, 'Validation isFloat on name failed');
     });
   });
+
+  describe('Boundary Value Analysis', () => {
+    it('should throw error when Ticket price is negative', async () => {
+      const ticket = {
+        price: -1.0,
+        departureFlight: flight.id,
+      };
+
+      models.Ticket.create(ticket).catch((response) => {
+        const [error] = response.errors;
+        assert.equal(error.type, 'Validation error');
+        assert.equal(error.message, 'Ticket Price cannot be less than zero');
+      });
+    });
+
+    it('should accept a Ticket price of zero', async () => {
+      const ticket = {
+        price: 0.0,
+        departureFlight: flight.id,
+      };
+
+      const result = await models.Ticket.create(ticket);
+      assert.isOk(result);
+    });
+
+    it('should accept a Ticket price of 1.0', async () => {
+      const ticket = {
+        price: 1.0,
+        departureFlight: flight.id,
+      };
+
+      const result = await models.Ticket.create(ticket);
+      assert.isOk(result);
+    });
+  });
 });
